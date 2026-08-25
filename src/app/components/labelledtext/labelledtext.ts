efd3bb6f81e1008d6dcbfbabd4ffff11d784b1da
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-25 14:50:38 +0300

    feat:add labelled text

--- src/app/components/labelledtext/labelledtext.ts
+++ src/app/components/labelledtext/labelledtext.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core'
+import { Text } from '../text/text'
+
+type Size = 'xs' | 'sm' | 'lg' | 'xl' | '2xl'
+type Color = 'green' | 'red' | 'yellow' | 'black'
 
 @Component({
   selector: 'app-labelledtext',
-  imports: [],
+  imports: [Text],
   templateUrl: './labelledtext.html',
-  styleUrl: './labelledtext.css',
+  styleUrl: './labelledtext.css'
 })
-export class Labelledtext {}
+export class Labelledtext {
+  label = input.required<string>()
+  value = input.required<string | number>()
+
+  label_size = input<Size>('sm')
+  label_color = input<Color>('black')
+
+  value_size = input<Size>('sm')
+  value_color = input<Color>('black')
+
+  styles(): string {
+    return `
+      w-full
+      flex
+      flex-row
+      items-center
+      justify-between
+      gap-4
+    `
+  }
+}
