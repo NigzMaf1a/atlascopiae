fcef8eb47ff556dd94b294b8b97bf4b9f50403c5
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-25 14:00:39 +0300

    feat:added text component

--- src/app/components/text/text.ts
+++ src/app/components/text/text.ts
@@ -1,9 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core'
+
+type Size = 'xs' | 'sm' | 'lg' | 'xl' | '2xl'
+type Color = 'green' | 'red' | 'yellow' | 'black'
 
 @Component({
   selector: 'app-text',
   imports: [],
   templateUrl: './text.html',
-  styleUrl: './text.css',
+  styleUrl: './text.css'
 })
-export class Text {}
+export class Text {
+  text = input.required<string | number>()
+  text_size = input<Size>('sm')
+  text_color = input<Color>('black')
+
+  styles(): string {
+    const s = this.text_size() === 'xs'
+      ? 'text-xs'
+      : this.text_size() === 'lg'
+        ? 'text-lg'
+        : this.text_size() === 'xl'
+          ? 'text-xl'
+          : this.text_size() === '2xl'
+            ? 'text-2xl'
+            : 'text-sm'
+
+    const c = this.text_color() === 'green'
+      ? 'text-green-500'
+      : this.text_color() === 'red'
+        ? 'text-red-500'
+        : this.text_color() === 'yellow'
+          ? 'text-yellow-500'
+          : 'text-black'
+
+    const extras = `
+    font-sans
+    font-normal
+    leading-normal
+    tracking-normal
+    antialiased
+  `
+
+    return `${s} ${c} ${extras}`
+  }
+}
