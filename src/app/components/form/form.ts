7ecbf823dfe298e3851bac4b61cfb1aaae224918
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-25 17:10:19 +0300

    feat:Wired the login page

--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,16 +1,34 @@
-import { Component, output } from '@angular/core'
+import {
+  Component,
+  input,
+  output
+} from '@angular/core'
+
+import {
+  FormGroup,
+  FormGroupDirective,
+  ReactiveFormsModule
+} from '@angular/forms'
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './form.html',
-  styleUrl: './form.css'
+  styleUrl: './form.css',
+  viewProviders: [
+    FormGroupDirective
+  ]
 })
 export class Form {
+
+  formGroup = input.required<FormGroup>()
   submitted = output<void>()
 
   submit(event: SubmitEvent): void {
     event.preventDefault()
+
     this.submitted.emit()
   }
 
--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core'
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms'
+
 import { Page } from '../../components/page/page'
+import { Form } from '../../components/form/form'
+import { Input } from '../../components/input/input'
+import { Button } from '../../components/button/button'
+import { Text } from '../../components/text/text'
 
 @Component({
   selector: 'app-login',
-  imports: [Page],
+  imports: [
+    ReactiveFormsModule,
+    Page,
+    Form,
+    Input,
+    Button,
+    Text
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
-export class Login { }
+export class Login {
+
+  form = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.email
+      ]
+    }),
+
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(8)
+      ]
+    })
+  })
+
+  submitting = false
+
+  submit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched()
+      return
+    }
+
+    this.submitting = true
+
+    const credentials = this.form.getRawValue()
+
+    console.log(credentials)
+
+    // Authentication request goes here.
+  }
+}
