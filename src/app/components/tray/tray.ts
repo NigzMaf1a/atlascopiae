dc6e1deefeebb2c62dc672915109333162fae1df
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-09-03 18:20:56 +0300

    feat:patching the tray component for production

--- src/app/components/tray/tray.ts
+++ src/app/components/tray/tray.ts
@@ -1,4 +1,9 @@
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, input } from '@angular/core'
+
+interface Styles {
+  cont: string
+  text: string
+}
 
 @Component({
   selector: 'app-tray',
@@ -7,5 +12,20 @@ import { Component, signal, inject } from '@angular/core';
   styleUrl: './tray.css',
 })
 export class Tray {
+  data = input.required<unknown[]>()
+  no_data_message = input.required<string>()
 
+  styles(): Styles {
+    const flex = this.data.length > 0 ? 'gap-2 flex-col' : 'flex-col justify-center items-center'
+    const borders = this.data.length > 0 ? 'border-neutral-300 rounded-lg' : 'border-none rounded-none'
+    const text_color = this.data.length > 0 ? 'text-green-500' : 'text-red-500'
+
+    const dim = 'w-full h-[300px]'
+    const text_styles = 'text-sm leading-tight tracking-tight'
+
+    return {
+      cont: `flex ${flex} ${borders} ${dim}`,
+      text: `${text_color} ${text_styles}`
+    }
+  }
 }
