8fd9ba7a55a6d81be1c2a9ba0ee2c6bf33112012
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-09-08 12:14:31 +0300

    feat: add manager nav styles

---
+++ src/app/service/styles/menu-styles.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core'
+
+interface Body {
+  menu: string
+  menuitem: string
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MenuStyles {
+  bodyStyles(): Body {
+    const menu_dim = 'h-[400px] w-[250px]'
+
+    const i_dim = ''
+
+    return {
+      menu: `${menu_dim}`,
+      menuitem: `${i_dim}`
+    }
+  }
+}
